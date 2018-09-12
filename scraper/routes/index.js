c922c1fdad420e5b0d95ef9fef921b40ff5834ba
Manish Pal <manish@intystep.in>
2018-09-12 08:25:04 +0530

    adding a contorller

--- scraper/routes/index.js
+++ scraper/routes/index.js
@@ -1,3 +1,5 @@
 const express = require('express');
 const router = express.Router();
+const brcData = require('../controllers/brcdata/index');
+router.use('/getdata', brcData.index);
 module.exports = router;
