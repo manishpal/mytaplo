e4ab8249b53541378d581b205fb379f3e1f95e72
Manish Pal <manish@intystep.in>
2018-09-12 13:34:04 +0530

    fixing routes

--- scraper/routes/index.js
+++ scraper/routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const router = express.Router();
 const brcData = require('../controllers/brcdata/index');
-router.get('/seawise/getdata', brcData.index);
+router.get('/getdata', brcData.index);
 module.exports = router;
